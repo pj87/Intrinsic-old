6d9ce4324ebb21508bc65837b7f3ace3703fa46c
pj87 <p.k.jastrzebski@gmail.com>
2018-07-15 16:13:47 +0200

    Adding marching cubes.

---
+++ InstrinsicAlgorithms/MarchingCubes.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+
+struct Vec3
+{
+	float x, y, z;
+};
+
+struct Triangle
+{
+	Vec3 pos;
+	Vec3 normal;
+	Vec3 color;
+};
+
+void vDrawScene();
+void vSetTime(float fTime);
+void vMarchingCubes(std::vector<Triangle>& triangles);
+void vMarchCube(float fX, float fY, float fZ, float fScale, std::vector<Triangle>& triangles);
