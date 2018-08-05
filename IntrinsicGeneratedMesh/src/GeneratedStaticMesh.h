abd3756239b37c672453138c77cdc8200cad9bda
pj87 <p.k.jastrzebski@gmail.com>
2018-08-05 19:14:09 +0200

    Adding a new seperate project for mesh generation.

---
+++ IntrinsicGeneratedMesh/src/GeneratedStaticMesh.h
@@ -0,0 +1,30 @@
+#include <vector>
+#include <string>
+#include <glm/glm.hpp>
+
+#pragma once
+
+namespace Intrinsic
+{
+namespace Core
+{
+namespace Resources
+{
+	typedef struct
+	{
+        std::string name;
+		std::vector<glm::vec3> positions; 
+		std::vector<glm::vec2> uv0; 
+		std::vector<glm::vec3> normals; 
+		std::vector<glm::vec3> tangents; 
+		std::vector<glm::vec3> binormals; 
+		std::vector<glm::vec4> colors; 
+		std::vector<int> indices;
+		std::string material;
+
+	} GeneratedStaticMesh;
+
+	static GeneratedStaticMesh _generatedStaticMeshes;
+}
+}
+};
