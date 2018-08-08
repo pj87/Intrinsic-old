9f76b4386531360954ffe43c25e686fe017fe784
pj87 <p.k.jastrzebski@gmail.com>
2018-08-08 19:32:29 +0200

    Adding multpile meshes but it doesn't work yet.

--- IntrinsicGeneratedMesh/src/GeneratedStaticMesh.cpp
+++ IntrinsicGeneratedMesh/src/GeneratedStaticMesh.cpp
@@ -56,8 +56,57 @@ void AddPlane(
   p_Meshes.push_back(mesh);
 }
 
+void AddMesh1(
+    std::vector<Intrinsic::Core::Resources::GeneratedStaticMesh>& p_Meshes)
+{
+  Intrinsic::Core::Resources::GeneratedStaticMesh mesh;
+
+  mesh.name = "PJGeneratedMesh1";
+  mesh.material = "default";
+
+  mesh.positions.push_back(glm::vec3(50.0, 0.0, -50.0));
+  mesh.positions.push_back(glm::vec3(-50.0, 0.0, -50.0));
+  mesh.positions.push_back(glm::vec3(-50.0, 0.0, 50.0));
+  mesh.positions.push_back(glm::vec3(50.0, -50.0, 50.0));
+
+  mesh.uv0.push_back(glm::vec2(1.0, 1.0));
+  mesh.uv0.push_back(glm::vec2(0.0, 1.0));
+  mesh.uv0.push_back(glm::vec2(0.0, 0.0));
+  mesh.uv0.push_back(glm::vec2(1.0, 0.0));
+
+  mesh.normals.push_back(glm::vec3(0.0, 1.0, 0.0));
+  mesh.normals.push_back(glm::vec3(0.0, 1.0, 0.0));
+  mesh.normals.push_back(glm::vec3(0.0, 1.0, 0.0));
+  mesh.normals.push_back(glm::vec3(0.0, 1.0, 0.0));
+
+  mesh.tangents.push_back(glm::vec3(1.0, 0.0, 0.0));
+  mesh.tangents.push_back(glm::vec3(1.0, 0.0, 0.0));
+  mesh.tangents.push_back(glm::vec3(1.0, 0.0, 0.0));
+  mesh.tangents.push_back(glm::vec3(1.0, 0.0, 0.0));
+
+  mesh.binormals.push_back(glm::vec3(0.0, 0.0, -1.0));
+  mesh.binormals.push_back(glm::vec3(0.0, 0.0, -1.0));
+  mesh.binormals.push_back(glm::vec3(0.0, 0.0, -1.0));
+  mesh.binormals.push_back(glm::vec3(0.0, 0.0, -1.0));
+
+  mesh.colors.push_back(glm::vec4(1.0, 1.0, 1.0, 1.0));
+  mesh.colors.push_back(glm::vec4(1.0, 1.0, 1.0, 1.0));
+  mesh.colors.push_back(glm::vec4(1.0, 1.0, 1.0, 1.0));
+  mesh.colors.push_back(glm::vec4(1.0, 1.0, 1.0, 1.0));
+
+  mesh.indices.push_back(0);
+  mesh.indices.push_back(1);
+  mesh.indices.push_back(2);
+  mesh.indices.push_back(0);
+  mesh.indices.push_back(2);
+  mesh.indices.push_back(3);
+
+  p_Meshes.push_back(mesh);
+}
+
 void Intrinsic::Core::Resources::MeshGenerator::GenerateMeshes()
 {
-  Intrinsic::Core::Resources::MeshGenerator::meshes.resize(2);
+  //Intrinsic::Core::Resources::MeshGenerator::meshes.resize(2);
   AddPlane(Intrinsic::Core::Resources::MeshGenerator::meshes);
+  AddMesh1(Intrinsic::Core::Resources::MeshGenerator::meshes);
 }
