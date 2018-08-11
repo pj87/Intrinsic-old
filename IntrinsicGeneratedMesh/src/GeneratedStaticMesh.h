6102f2d4413aaa6f191310d697e47e0f91575144
pj87 <p.k.jastrzebski@gmail.com>
2018-08-11 09:52:12 +0200

    Started adding a singleton and probably something as factory in one.

--- IntrinsicGeneratedMesh/src/GeneratedStaticMesh.h
+++ IntrinsicGeneratedMesh/src/GeneratedStaticMesh.h
@@ -37,6 +37,36 @@ namespace Resources
 
 			static void GenerateMeshes();
 	};
+
+	// Taken from https://stackoverflow.com/questions/1008019/c-singleton-design-pattern
+
+	class S
+        {
+        public:
+          static S& getInstance()
+          {
+            static S instance; // Guaranteed to be destroyed.
+                               // Instantiated on first use.
+            return instance;
+          }
+
+        private:
+          S() {} // Constructor? (the {} brackets) are needed here.
+
+          // C++ 11
+          // =======
+          // We can use the better technique of deleting the methods
+          // we don't want.
+        public:
+          S(S const&) = delete;
+          void operator=(S const&) = delete;
+
+          // Note: Scott Meyers mentions in his Effective Modern
+          //       C++ book, that deleted functions should generally
+          //       be public as it results in better error messages
+          //       due to the compilers behavior to check accessibility
+          //       before deleted status
+        };
 }
 }
 };
