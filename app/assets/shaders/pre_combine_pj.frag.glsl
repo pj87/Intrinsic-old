d90211ad5280d8ebcf3f7c57dcfc6487ff578ab6
pj87 <p.k.jastrzebski@gmail.com>
2018-11-07 21:12:46 +0100

    Probably the last stage of removal, without any warnings.

--- app/assets/shaders/pre_combine_pj.frag.glsl
+++ app/assets/shaders/pre_combine_pj.frag.glsl
@@ -14,10 +14,6 @@
 
 #version 450
 
-//#extension GL_ARB_separate_shader_objects : enable
-//#extension GL_ARB_shading_language_420pack : enable
-//#extension GL_GOOGLE_include_directive : enable
-
 layout(binding = 0) uniform sampler2D albedoTex;
 layout(binding = 1) uniform sampler2D albedoTranspTex;
 
