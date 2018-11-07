2b18d00b24e0718cffdd1013f3040f0b88f2e982
pj87 <p.k.jastrzebski@gmail.com>
2018-11-07 20:52:22 +0100

    Further simplifying.

--- app/assets/shaders/pre_combine_pj.frag.glsl
+++ app/assets/shaders/pre_combine_pj.frag.glsl
@@ -14,22 +14,12 @@
 
 #version 450
 
-#extension GL_ARB_separate_shader_objects : enable
-#extension GL_ARB_shading_language_420pack : enable
-#extension GL_GOOGLE_include_directive : enable
-
-#include "lib_math.glsl"
-#include "lib_buffers.glsl"
-#include "lib_vol_lighting.glsl"
-#include "ubos.inc.glsl"
-#include "lib_lighting.glsl"
-
-//PER_INSTANCE_DATA_PRE_COMBINE;
+//#extension GL_ARB_separate_shader_objects : enable
+//#extension GL_ARB_shading_language_420pack : enable
+//#extension GL_GOOGLE_include_directive : enable
 
 layout(binding = 0) uniform sampler2D albedoTex;
-layout(binding = 1) uniform sampler2D normalTex;
-layout(binding = 2) uniform sampler2D param0Tex;
-layout(binding = 3) uniform sampler2D albedoTranspTex;
+layout(binding = 1) uniform sampler2D albedoTranspTex;
 
 layout(location = 0) in vec2 inUV0;
 layout(location = 0) out vec4 outColor;
