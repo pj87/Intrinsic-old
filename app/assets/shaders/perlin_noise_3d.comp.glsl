01d8fcc3bdebfd471b02d3e3e57c5c0e4d698d11
pj87 <p.k.jastrzebski@gmail.com>
2019-02-20 21:38:33 +0100

    Fixed the colors.

--- app/assets/shaders/perlin_noise_3d.comp.glsl
+++ app/assets/shaders/perlin_noise_3d.comp.glsl
@@ -281,8 +281,8 @@ float map3( in vec3 p)
 
 float mapScaled(vec3 p, vec4 c)
 {
-	//return map2(p/0.0025f, c) * 0.0025f;
-	return map3(p/0.0025f) * 0.0025f;
+	return map2(p/0.0025f, c) * 0.0025f;
+	//return map3(p/0.0025f) * 0.0025f;
 }
 
 layout(local_size_x = 8u, local_size_y = 8u, local_size_z = 8u) in;
