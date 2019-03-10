cbe517a0a656204ca075c0e8300252d0a775af50
pj87 <p.k.jastrzebski@gmail.com>
2019-03-10 18:40:49 +0100

    Added the first temple.

--- app/assets/shaders/mandelbulb.comp.glsl
+++ app/assets/shaders/mandelbulb.comp.glsl
@@ -519,6 +519,32 @@ float dist2nearest(vec3 p)
 	return sdCross(q * 27.) / 27.;
 }
 
+// k should be negative. -4.0 works nicely.
+float smin(float a, float b, float k)
+{
+	// I'm guessing that base 2 operations are the fastest.
+	return log2(exp2(k*a)+exp2(k*b))/k;
+}
+
+vec3 RotateY(vec3 v, float rad)
+{
+	float cos = cos(rad);
+	float sin = sin(rad);
+	//if (RIGHT_HANDED_COORD)
+	return vec3(cos * v.x - sin * v.z, v.y, sin * v.x + cos * v.z);
+	//else return new float3(cos * x + sin * z, y, -sin * x + cos * z);
+}
+
+float dSphere(vec3 p, float rad)
+{
+	return length(p) - rad;
+}
+
+float dBox(vec3 pos, vec3 b)
+{
+	return length(max(abs(pos)-(b),0.0));
+}
+
 float sdCapsule( vec3 p, vec3 a, vec3 b, float r )
 {
 	vec3 pa = p-a, ba = b-a;
@@ -535,14 +561,102 @@ float dBoxSigned(vec3 p)
 	return min(max(d.x,max(d.y,d.z)),0.0) + length(max(d,0.0));
 }
 
-float dSphere(vec3 p, float rad)
+float dBoxSlant(vec3 p, vec3 b)
 {
-	return length(p) - rad;
+	float size = b.x;
+	float f = length(max(abs(p)-(b),0.0));
+	//float f = length(max(abs(p.x)+abs(p.z)-(b),0.0));
+	//f = max(f, p.y*0.5 + abs(p.x) + abs(p.z) - size * 0.995);
+	return f;
 }
 
 float dFloor(vec3 p)
 {
 	return p.y + 1.0;
+}
+
+float sdColumn( vec3 p, vec3 c )
+{
+	float cyl = length(p.xz-c.xy)-c.z;// + abs(p.y);
+	cyl -= cos(p.y*2.0)*0.045;
+	float a = atan(p.x - c.x, p.z - c.y);
+	a /= 2.0*PI;
+	float subs = 48.0;
+	a *= subs;
+	//cyl *= pow(sin(a), 0.5) * 0.925 + 1.0;
+	cyl += abs(sin(a)) * 0.015;
+
+	cyl = max(cyl, p.y - 2.4);
+	cyl = min(cyl, dBox(p + vec3(0.0, 1.0, 0.0), vec3(0.3, 0.2, 0.3)));
+	cyl = smin(cyl, dBoxSlant(RotateY(p, PI/4.0) + vec3(0.0, -2.3, 0.0), vec3(0.3, 0.15, 0.3)), -24.0);
+	//cyl = min(cyl, dBox(RotateY(p, PI/4.0) + vec3(0.0, -2.3, 0.0), vec3(0.3, 0.15, 0.3)));
+	return cyl;
+}
+
+float length16(vec2 v)
+{
+	return pow(pow(abs(v.x),16.0) + pow(abs(v.y), 16.0), 1.0/16.0);
+	//return pow((pow(v.x,16.0) + pow(v.y, 16.0)), (1.0/16.0));
+}
+
+float globalRadial;
+
+float sdTorusBricks( vec3 p, vec2 t, vec3 center, float subs )
+{
+	p -= center;
+	float a = globalRadial;// + PI/6.0;// atan(p.x, p.z);
+	a = pow(abs(sin(a*subs)), 0.25);
+	//a = mod(a,PI*2.0) - 0.5;
+	//a = a *0.025 + 0.975;
+	a = a *0.2 + 0.8;
+	vec2 q = vec2(length(p.xz)-t.x,p.y);
+	return length16(q)-t.y*a;
+}
+float sdTorusArch( vec3 p, vec2 t, vec3 center, float subs )
+{
+	p -= center;
+	float a = atan(p.y, p.z);
+	a = pow(abs(sin(a*subs)), 0.25);
+	//a = mod(a,PI*2.0) - 0.5;
+	//a = a *0.025 + 0.975;
+	a = a *0.25 + 0.75;
+	p.y -= cos(p.y)*0.4;
+	vec2 q = vec2(length(p.yz)-t.x,p.x);
+	return length16(q)-t.y*a;
+}
+
+// This makes the stained glass windows in the dome.
+float dTiles(vec3 p)
+{
+	float subs = 16.0;
+	float final = length(p) - 2.2;
+	float a = globalRadial;// atan(p.x, p.z);
+	a /= 2.0*PI;
+	a *= subs;
+	a = abs((fract(a) - 0.5))*2.0;	// triangle wave from 0.0 to 1.0
+	a -= 0.15;
+	a *= 6.0;
+	a = max(0.0, a);
+	a = min(0.75, a);
+
+	float b = atan(length(p.xz), p.y);
+	b /= 2.0*PI;
+	b *= subs;
+	b = abs((fract(b) - 0.5))*2.0;	// triangle wave from 0.0 to 1.0
+	b -= 0.15;
+	b *= 6.0;
+	b = max(0.0, b);
+	b = min(0.75, b);
+	
+	a = a*b;
+
+	a = a *0.2 + 0.8;
+	b = b *0.2 + 0.8;
+	
+	final = final - a;
+	final = max(final, 0.5-p.y);
+	final = min(final, length(p.xz) - 0.5);	// oculus - cylinder in middle
+	return final/1.414;
 }
 
 float cathedral2(vec3 p)
@@ -558,8 +672,8 @@ float cathedral2(vec3 p)
 
 	float final = -sdCapsule(p, vec3(0.0,-0.5,0.0), vec3(0.0,2.25,0.0), 3.0);
 	// This if condition is for a culling speedup and a cool bevel effect on the ceiling tiles.
-	//if (final < 0.01) final = max(final, -dTiles(p + vec3(0.0, -2.25, 0.0)));
-	//final = min(final, sdTorusBricks(p, vec2(2.75, 0.25), vec3(0.0, -0.795, 0.0), 12.0));
+	if (final < 0.01) final = max(final, -dTiles(p + vec3(0.0, -2.25, 0.0)));
+	final = min(final, sdTorusBricks(p, vec2(2.75, 0.25), vec3(0.0, -0.795, 0.0), 12.0));
 	final = max(final, -sdCapsule(p, vec3(-6.0,0.0,0.0), vec3(6.0,0.0,0.0), 2.0));
 	final = max(final, -sdCapsule(p, vec3(0.0,0.0,-16.0), vec3(0.0,0.0,16.0), 2.0));
 	final = max(final, -dBoxSigned(p));
