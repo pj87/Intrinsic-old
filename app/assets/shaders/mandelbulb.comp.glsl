9f867ca8e2531b644d14ac48e062ef8c3d20ebdc
pj87 <p.k.jastrzebski@gmail.com>
2019-03-16 12:06:03 +0100

    Added a textured box. Now, trying to add the proper three-planar mapping.

--- app/assets/shaders/mandelbulb.comp.glsl
+++ app/assets/shaders/mandelbulb.comp.glsl
@@ -723,7 +723,7 @@ void main()
     uvec3 id = gl_GlobalInvocationID;
 	vec3 uv = vec3(id);
 
-	uv *= 0.0001;
+	//uv *= 0.0001;
 
 	//uncomment this for fractal noise
 	//float n = fBm(uv, 4);
@@ -736,10 +736,11 @@ void main()
 
 	vec4 c = 0.45*cos( vec4(0.5,3.9,1.4,1.1) + _Frequency*vec4(1.2,1.7,1.3,2.5) ) - vec4(0.3,0.0,0.0,0.0);
 	//vec4 c = vec4(0.4);
-	_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -mapScaled(uv - vec3(0.002f), c);
+	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -mapScaled(uv - vec3(0.002f), c);
 	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -mapScaled(uv - vec3(100.0f * sin(_Frequency)), c);
 	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -sdTorus(uv - vec3(_Frequency), vec2(5.0, 2.0));
 	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -sdBox(uv - vec3(10.0), vec3(10.0, 5.0, 5.0));
+	_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -sdBox(uv - vec3(10.0f), vec3(10.0, 10.0, 10.0));
 	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -sdSphere(uv - vec3(10.0), 10.0);
 
 }
