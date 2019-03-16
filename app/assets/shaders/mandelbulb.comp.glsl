47a5807cec315762be11bd1389c583627427c3d4
pj87 <p.k.jastrzebski@gmail.com>
2019-03-16 13:30:07 +0100

    Changed the resolution and now the cathdray doesn't look good. For now, I need to focus on the tetxuring.

--- app/assets/shaders/mandelbulb.comp.glsl
+++ app/assets/shaders/mandelbulb.comp.glsl
@@ -712,9 +712,9 @@ float mapScaled(vec3 p, vec4 c)
 	
 	//return sponge2(p / 0.001) * 0.001;
 	
-	//return cathedral2(p / 0.0005) * 0.0005;
+	return cathedral2(p / 3.0) * 3.0;
 	
-	return sdPlane(p);
+	//return sdPlane(p);
 }
 
 layout(local_size_x = 8u, local_size_y = 8u, local_size_z = 8u) in;
@@ -736,11 +736,11 @@ void main()
 
 	vec4 c = 0.45*cos( vec4(0.5,3.9,1.4,1.1) + _Frequency*vec4(1.2,1.7,1.3,2.5) ) - vec4(0.3,0.0,0.0,0.0);
 	//vec4 c = vec4(0.4);
-	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -mapScaled(uv - vec3(0.002f), c);
+	_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -mapScaled(uv - vec3(5.0f), c);
 	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -mapScaled(uv - vec3(100.0f * sin(_Frequency)), c);
 	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -sdTorus(uv - vec3(_Frequency), vec2(5.0, 2.0));
 	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -sdBox(uv - vec3(10.0), vec3(10.0, 5.0, 5.0));
-	_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -sdBox(uv - vec3(10.0f), vec3(10.0, 10.0, 10.0));
+	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -sdBox(uv - vec3(10.0f), vec3(10.0, 10.0, 10.0));
 	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -sdSphere(uv - vec3(10.0), 10.0);
 
 }
