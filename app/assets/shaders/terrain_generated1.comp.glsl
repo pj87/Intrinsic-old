1604dcb42baef1a8317a9a73229e2806112084db
pj87 <p.k.jastrzebski@gmail.com>
2019-04-28 13:04:25 +0200

    Really decent terrain.

--- app/assets/shaders/terrain_generated1.comp.glsl
+++ app/assets/shaders/terrain_generated1.comp.glsl
@@ -31,18 +31,18 @@ float noi( in vec2 p )
 
 float terrainMed( vec2 p )
 {
-    p *= 0.0013;
+    p *= 0.0035;
 
     float s = 1.0;
 	float t = 0.0;
-	for( int i=0; i<5; i++ )
+	for( int i=0; i<3; i++ )
 	{
         t += s*noi( p );
-		s *= 0.5 + 0.1*t;
-        p = 0.97*m2*p + (t-0.5)*0.2;
+		s *= 0.5;// + 0.1*t;
+        p = 0.97*m2*p;// + (t-0.5)*0.2;
 	}
             
-    return t*55.0;
+    return t*35.0;
 }
 
 float map( in vec3 pos, float time )
@@ -64,7 +64,8 @@ float map( in vec3 pos, float time )
 
 float mapScaled(vec3 p, vec4 c)
 {
-	return map(p / 0.01, 0.0) * 0.01;
+	return map(p / 0.1, 0.0) * 0.1;
+	//return length(p) - 20.0;
 }
 
 layout(local_size_x = 8u, local_size_y = 8u, local_size_z = 8u) in;
@@ -87,7 +88,8 @@ void main()
 	vec4 c = 0.45*cos( vec4(0.5,3.9,1.4,1.1) + _Frequency*vec4(1.2,1.7,1.3,2.5) ) - vec4(0.3,0.0,0.0,0.0);
 	//vec4 c = vec4(0.4);
 	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -mapScaled(uv - vec3(25.0f, 10.0f, 0.0f), c);
-	_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -mapScaled(uv - vec3(100.0f, 1.0f, 100.0f), c);
+	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -mapScaled(uv - vec3(100.0f, 1.0f, 100.0f), c);
+	_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -mapScaled(uv - vec3(32.0f, 32.0, 32.0f), c);
 	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -mapScaled(uv - vec3(100.0f * sin(_Frequency)), c);
 	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -sdTorus(uv - vec3(_Frequency), vec2(5.0, 2.0));
 	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -sdBox(uv - vec3(10.0), vec3(10.0, 5.0, 5.0));
