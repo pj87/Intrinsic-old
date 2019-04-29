9318e55b1d2da4e528cf0aebf1acd06abee0af80
pj87 <p.k.jastrzebski@gmail.com>
2019-04-29 14:34:05 +0200

    Added first version of fake trees and it works!

--- app/assets/shaders/terrain_generated1.comp.glsl
+++ app/assets/shaders/terrain_generated1.comp.glsl
@@ -22,6 +22,21 @@ layout(binding = 4) buffer _VoxelBuffer
 	float _Result[];
 };
 
+#define pmod(a,b)    ( mod(mod((a),(b))+(b),(b)) )
+#define rep(a,r)    ( pmod(((a)+(r)*.5),(r))-(r)*.5 )
+#define repxz(a,r)    vec3( rep((a).x,(r)), (a).y, rep((a).z,(r)) )
+
+float opU(float d1, float d2)
+{
+	return (d1 < d2) ? d1 : d2;
+}
+
+float sdBox( vec3 p, vec3 b )
+{
+    vec3 d = abs(p) - b;
+    return min(max(d.x,max(d.y,d.z)),0.0) + length(max(d,0.0));
+}
+
 const mat2 m2 = mat2(1.6,-1.2,1.2,1.6);
 
 float noi( in vec2 p )
@@ -49,14 +64,20 @@ float map( in vec3 pos, float time )
 {
     float m = 0.0;
 	float h = pos.y - terrainMed(pos.xz);
-
+	
+	/*
     float sph = 100.0;
     float k = 60.0;
     float w = clamp( 0.5 + 0.5*(h-sph)/k, 0.0, 1.0 );
     h = mix( h, sph, w ) - k*w*(1.0-w);
     m = mix( m, 1.0, w ) - 1.0*w*(1.0-w);
     m = clamp(m,0.0,1.0);
-
+	*/
+	
+	pos = repxz(vec3(pos.x, h, pos.z), 30.0);
+    
+    h = opU(h, sdBox(pos, vec3(2.5, 50.0, 2.5)));
+	
 	return h;
 	
     //return vec2( h, m );
