bfdff1c3e21e4c6dc9fb7572063e784a3cf8d84d
pj87 <p.k.jastrzebski@gmail.com>
2019-06-16 14:55:12 +0200

    Added memset instead of memcpy and it stopped shitting with triangles.

--- IntrinsicRenderer/src/IntrinsicRendererResourcesBuffer.cpp
+++ IntrinsicRenderer/src/IntrinsicRendererResourcesBuffer.cpp
@@ -143,13 +143,15 @@ void BufferManager::updateResources(const BufferRefArray& p_Buffers)
           _INTR_LOG_INFO("PJ: Changed: %f, %f, %f", pos1->x, pos1->y, pos1->z);
         }
       }
-	  */
-
+		
       {
 
         memcpy(stagingGpuAllocInfo._mappedMemory, initialData,
                _descSizeInBytes(bufferRef));
       }
+	  */
+
+	  memset(stagingGpuAllocInfo._mappedMemory, 0, _descSizeInBytes(bufferRef));
 
       VkBufferCopy bufferCopy = {};
       {
