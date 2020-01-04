a252af678d7599096868aa03dc27e37e6c813c48
pj87 <p.k.jastrzebski@gmail.com>
2020-01-04 14:14:01 +0100

    TEMPLE WORKS!!!!

--- app/assets/shaders/temple_ruins.comp.glsl
+++ app/assets/shaders/temple_ruins.comp.glsl
@@ -123,9 +123,9 @@ float mapScaled(vec3 p, vec4 c)
 	//return sponge2(p / 0.0025) * 0.0025;
 	
 	//return ruins(p/0.000025f) * 0.000025f;
-	//return ruins(p/1.0f) * 1.0f;
+	return ruins(p/1.0f) * 1.0f;
 	
-	return temp(p/1.0f) * 1.0f;
+	//return temp(p/1.0f) * 1.0f;
 }
 
 layout(local_size_x = 8u, local_size_y = 8u, local_size_z = 8u) in;
