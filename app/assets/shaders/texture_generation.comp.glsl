1ae76cbf4d2f0db6f86d592d35c0fcd9a883ae9f
pj87 <p.k.jastrzebski@gmail.com>
2020-01-12 14:30:30 +0100

    Forgot to add this line in the previous commit.

--- app/assets/shaders/texture_generation.comp.glsl
+++ app/assets/shaders/texture_generation.comp.glsl
@@ -4,7 +4,7 @@ layout(binding = 0, RGBA8) uniform image2D _TextureTex;
 layout(binding = 1, RG8) uniform image2D _NormalTex;
 layout(binding = 2, RG8) uniform image2D _PBRTex;
 
-layout(local_size_x = 8u, local_size_y = 8u, local_size_z = 8u) in;
+layout(local_size_x = 1u, local_size_y = 1u, local_size_z = 1u) in;
 void main()
 {
     ivec3 id = ivec3(gl_GlobalInvocationID);
