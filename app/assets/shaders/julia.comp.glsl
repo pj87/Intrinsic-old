dfa851cca2139459a2a2a77dac70d83ab2cc7fd1
pj87 <p.k.jastrzebski@gmail.com>
2020-02-08 12:49:24 +0100

    Fixed julia's scaling and uv's issues. The look of julia didn't change.

--- app/assets/shaders/julia.comp.glsl
+++ app/assets/shaders/julia.comp.glsl
@@ -286,7 +286,7 @@ float map3( in vec3 p)
 
 float mapScaled(vec3 p, vec4 c)
 {
-	return map2(p/0.0025f, c) * 0.0025f;
+	return map2(p/25.0f, c) * 25.0f;
 	//return map3(p/0.0025f) * 0.0025f;
 }
 
@@ -296,7 +296,7 @@ void main()
     uvec3 id = gl_GlobalInvocationID;
 	vec3 uv = vec3(id);
 
-	uv *= 0.0001;
+	//uv *= 1.0;
 
 	//uncomment this for fractal noise
 	//float n = fBm(uv, 4);
@@ -309,7 +309,7 @@ void main()
 
 	vec4 c = 0.45*cos( vec4(0.5,3.9,1.4,1.1) + _Frequency*vec4(1.2,1.7,1.3,2.5) ) - vec4(0.3,0.0,0.0,0.0);
 	//vec4 c = vec4(0.4);
-	_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -mapScaled(uv - vec3(0.002f), c);
+	_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -mapScaled(uv - vec3(20.0f), c);
 	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -mapScaled(uv - vec3(100.0f * sin(_Frequency)), c);
 	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -sdTorus(uv - vec3(_Frequency), vec2(5.0, 2.0));
 	//_Result[id.x + id.y * _Width + id.z * _Width * _Height] = -sdBox(uv - vec3(10.0), vec3(10.0, 5.0, 5.0));
