371b63a6851b686acf18e399b8c5dfd96244cb3c
pj87 <p.k.jastrzebski@gmail.com>
2020-02-15 20:59:47 +0100

    Added explosion.

--- IntrinsicRenderer/src/IntrinsicRendererRenderSystem.cpp
+++ IntrinsicRenderer/src/IntrinsicRendererRenderSystem.cpp
@@ -224,7 +224,14 @@ void RenderSystem::init(void* p_PlatformHandle, void* p_PlatformWindow)
 		"normal_generation.comp",
         "geometry_generation_new.comp", 
 		false);
-
+     
+	RenderPass::DynamicGeometryGeneration::addDynamicGeneradtedMesh(
+        64, 64, 64,
+        _N(house_5), 
+		"explosion.comp", 
+		"normal_generation.comp",
+        "geometry_generation_new.comp");
+	
 	RenderPass::DynamicGeometryGeneration::addDynamicGeneradtedMesh(
         64, 64, 64, 
 		_N(terrain), 
