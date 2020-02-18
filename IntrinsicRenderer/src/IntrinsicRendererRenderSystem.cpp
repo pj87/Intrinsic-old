dbbeea86ea306f87a7c49ab9bf02f28674e19ac9
pj87 <p.k.jastrzebski@gmail.com>
2020-02-18 15:39:10 +0100

    Added more exploding objects to Lava scene.

--- IntrinsicRenderer/src/IntrinsicRendererRenderSystem.cpp
+++ IntrinsicRenderer/src/IntrinsicRendererRenderSystem.cpp
@@ -257,7 +257,7 @@ void RenderSystem::init(void* p_PlatformHandle, void* p_PlatformWindow)
     RenderPass::DynamicGeometryGeneration::addDynamicGeneradtedMesh(
         64, 64, 64, 
 		_N(terrain_lava), 
-		"terrain_tatooine_generated.comp", 
+		"terrain_lava_generated.comp", 
 		"normal_generation.comp",
         "geometry_generation_new.comp", 
 		false);
