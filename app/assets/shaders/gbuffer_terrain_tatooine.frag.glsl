8152763b55339017b8f6d018c4f4c6d897ae9553
pj87 <p.k.jastrzebski@gmail.com>
2020-05-13 20:01:23 +0200

    Improved the tatooine terrain.

--- app/assets/shaders/gbuffer_terrain_tatooine.frag.glsl
+++ app/assets/shaders/gbuffer_terrain_tatooine.frag.glsl
@@ -99,38 +99,26 @@ void main()
   const vec2 uv0 = UV0_TRANSFORM_ANIMATED(inUV0);
   const vec2 uv0Raw = UV0(inUV0);
 
-  const vec4 albedo0 = vec4(tex3D(inPosition * 0.25, inNormalTPM, albedoTex0), 1.0);
-  const vec3 normal0 = tex3DNormal(inPosition * 0.25, inNormalTPM, normalTex0);
-  const vec3 pbr0 = tex3D(inPosition * 0.25, inNormalTPM, pbrTex0).rgg;
+  const vec4 albedo0 = vec4(tex3D(inPosition * 0.1, inNormalTPM, albedoTex0), 1.0);
+  const vec3 normal0 = tex3DNormal(inPosition * 0.1, inNormalTPM, normalTex0);
+  const vec3 pbr0 = tex3D(inPosition * 0.1, inNormalTPM, pbrTex0).rgg;
 
   const vec4 albedo1 = vec4(tex3D(inPosition * 1.0, inNormalTPM, albedoTex1), 1.0);
   const vec3 normal1 = tex3DNormal(inPosition * 1.0, inNormalTPM, normalTex1);
   const vec3 pbr1 = tex3D(inPosition * 1.0, inNormalTPM, pbrTex1).rgg;
 
-  const vec4 albedo2 = vec4(tex3D(inPosition * 0.25, inNormalTPM, albedoTex2), 1.0);
-  const vec3 normal2 = tex3DNormal(inPosition * 0.25, inNormalTPM, normalTex2);
-  const vec3 pbr2 = tex3D(inPosition * 0.25, inNormalTPM, pbrTex2).rgg;
+  const vec4 albedo2 = vec4(tex3D(inPosition * 1.25, inNormalTPM, albedoTex2), 1.0);
+  const vec3 normal2 = tex3DNormal(inPosition * 1.25, inNormalTPM, normalTex2);
+  const vec3 pbr2 = tex3D(inPosition * 1.25, inNormalTPM, pbrTex2).rgg;
 
-  float noise = clamp(tex3D(inPosition * 1.0, inNormalTPM, noiseTex).r, 0.0, 1.0);
-  vec3 blendMask = tex3DBlendMask(inPosition * 0.25, inNormalTPM, blendMaskTex).rgb;
+  float noise = clamp(tex3D(inPosition * 1.0, inNormalTPM, noiseTex).r, 0.0, 1.0) * 2.0;
+  vec3 blendMask = tex3DBlendMask(inPosition * 1.25, inNormalTPM, blendMaskTex).rgb;
   
-  vec3 albedo = blend(albedo1.rgb, albedo0.rgb, albedo2.rgb, blendMask, noise);
-  // Alternative version, very interesting:
-  //vec3 albedo = blend(albedo1.rgb, albedo0.rgb, albedo2.rgb, blendMask, noise * 100.0);
-  vec3 normal = blend(normal1.rgb, normal0.rgb, normal2.rgb, blendMask, noise);
-  vec2 pbr = blend(pbr1.rgb, pbr0.rgb, pbr2.rgb, blendMask, noise).rg;
+  //vec3 albedo = blend(albedo1.rgb * 1.0, albedo2.rgb * 4.0, albedo0.rgb * 4.0, blendMask, noise);
+  vec3 albedo = blend(albedo1.rgb * 2.0, albedo2.rgb * 4.0, albedo0.rgb * 3.0, blendMask, noise);
+  vec3 normal = blend(normal1.rgb * 1.0, normal2.rgb * 1.0, normal0.rgb * 1.0, blendMask, noise);
+  vec2 pbr = blend(pbr1.rgb * 1.0, pbr2.rgb * 1.0, pbr0.rgb * 1.0, blendMask, noise).rg;
   
-  /*
-  vec3 albedo = blend(albedo1.rgb * 1.0, albedo0.rgb * 0.0, albedo2.rgb * 0.0, blendMask, noise);
-  vec3 normal = blend(normal1.rgb, normal0.rgb, normal2.rgb, blendMask, noise);
-  vec2 pbr = blend(pbr1.rgb, pbr0.rgb, pbr2.rgb, blendMask, noise).rg;
-  */
-  //vec3 albedo = blend(albedo0.rgb * 1.0, albedo1.rgb * 1.0, albedo2.rgb * 1.0, blendMask, noise);
-  //vec3 albedo = albedo1.rgb;
-  //vec3 normal = vec3(0.0);
-  //vec2 pbr = vec2(0.0);
-  
-  /*
   float occlusion =
       clamp(mix(clamp(noise * 5.0, 0.0, 1.0) * blendMask.b, 1.0 - blendMask.r,
                 clamp((1.0 - blendMask.g) * 2.0 - 0.9, 0.0, 1.0)) *
@@ -138,7 +126,7 @@ void main()
                 0.2,
             0.0, 1.0);
   albedo *= occlusion;
-  */
+  
   GBuffer gbuffer;
   {
     gbuffer.albedo = vec4(albedo, 1.0) * uboPerInstance.colorTint;
